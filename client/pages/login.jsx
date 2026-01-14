adf2699db47f245297baf558b55ccf8ba940c4e0
Happiness Ngeete <ndolohappiness@gmail.com>
2026-01-14 16:56:03 +0300

    Remove mockLandlords import from login page

--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { useRouter } from "next/router"
 import Link from "next/link"
 import { toast } from "react-toastify"
-import { mockLandlords } from "../lib/mockData"
+
 
 export default function Login() {
   const router = useRouter()
